78d6d11444b84e36d6499e57b3386e5f9464ba18
Andrew F. Whitworth <awhitworth@asicentral.com>
2020-03-11 20:06:41 -0400

    v0.5.1
    Update build and publish scripts
    reorg solution since we have more projects

--- build.sh
+++ build.sh
@@ -1,5 +1,12 @@
 dotnet build -f netstandard2.0 Src/StoneFruit/StoneFruit.csproj --configuration Release
 dotnet pack StoneFruit.csproj --configuration Release --no-build --no-restore
 
-dotnet build -f netstandard2.0 Src/StoneFruit.StructureMap/StoneFruit.StructureMap.csproj --configuration Release
-dotnet pack Src/StoneFruit.StructureMap/StoneFruitStructureMap.csproj --configuration Release --no-build --no-restore
+dotnet build -f netstandard2.0 Src/StoneFruit.Containers.StructureMap/StoneFruit.Containers.StructureMap.csproj --configuration Release
+dotnet pack Src/StoneFruit.Containers.StructureMap/StoneFruit.Containers.StructureMap.csproj --configuration Release --no-build --no-restore
+
+dotnet build -f netstandard2.0 Src/StoneFruit.Containers.Lamar/StoneFruit.Containers.Lamar.csproj --configuration Release
+dotnet pack Src/StoneFruit.Containers.Lamar/StoneFruit.Containers.Lamar.csproj --configuration Release --no-build --no-restore
+
+dotnet build -f netstandard2.0 Src/StoneFruit.Containers.Ninject/StoneFruit.Containers.Ninject.csproj --configuration Release
+dotnet pack Src/StoneFruit.Containers.Ninject/StoneFruit.Containers.Ninject.csproj --configuration Release --no-build --no-restore
+
