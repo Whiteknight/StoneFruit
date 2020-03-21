3588380e2b82736055d5b960741e632041b80251
Andrew F. Whitworth <awhitworth@asicentral.com>
2020-03-21 07:28:28 -0400

    v0.6.0
    Small fix to build script

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 dotnet build -f netstandard2.0 Src/StoneFruit/StoneFruit.csproj --configuration Release
-dotnet pack StoneFruit.csproj --configuration Release --no-build --no-restore
+dotnet pack Src/StoneFruit/StoneFruit.csproj --configuration Release --no-build --no-restore
 
 dotnet build -f netstandard2.0 Src/StoneFruit.Containers.StructureMap/StoneFruit.Containers.StructureMap.csproj --configuration Release
 dotnet pack Src/StoneFruit.Containers.StructureMap/StoneFruit.Containers.StructureMap.csproj --configuration Release --no-build --no-restore
