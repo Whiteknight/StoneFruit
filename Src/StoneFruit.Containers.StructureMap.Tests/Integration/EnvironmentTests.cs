1da66d44b18573b928943b6abf357a127bb80ec8
Andrew F. Whitworth <awhitworth@asicentral.com>
2020-04-11 16:06:39 -0400

    Add a missing integration test for structuremap

---
+++ Src/StoneFruit.Containers.StructureMap.Tests/Integration/EnvironmentTests.cs
@@ -0,0 +1,23 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using TestUtilities;
+
+namespace StoneFruit.Containers.StructureMap.Tests.Integration
+{
+    public class EnvironmentTests
+    {
+        [Test]
+        public void Instance_Test()
+        {
+            var output = new TestOutput();
+            var engine = new EngineBuilder()
+                .SetupHandlers(h => h.UseStructureMapHandlerSource())
+                .SetupOutput(o => o.DoNotUseConsole().Add(output))
+                .SetupEnvironments(e => e.UseInstance(new TestEnvironment("Single")))
+                .Build();
+            engine.RunHeadless("test-environment");
+            output.Lines.Count.Should().Be(1);
+            output.Lines[0].Should().Be("Single");
+        }
+    }
+}
