585df20ffbcf8f4d0ceae28950b6b68c78db21d5
Andrew F. Whitworth <awhitworth@asicentral.com>
2020-05-05 19:11:29 -0400

    Unit tests for microsoft DI integration

---
+++ Src/StoneFruit.Containers.Microsoft.Tests/Integration/EnvironmentTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using FluentAssertions;
+using NUnit.Framework;
+using StoneFruit.Execution;
+using TestUtilities;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace StoneFruit.Containers.Microsoft.Tests.Integration
+{
+    public class EnvironmentTests
+    {
+        [Test]
+        public void Instance_Test()
+        {
+            var output = new TestOutput();
+            var services = new ServiceCollection();
+            IServiceProvider provider = null;
+            services.SetupEngine<TestEnvironment>(b => b
+                .SetupOutput(o => o.DoNotUseConsole().Add(output))
+                .SetupEnvironments(e => e.UseInstance(new TestEnvironment("Single"))),
+                () => provider
+            );
+            provider = services.BuildServiceProvider();
+            var engine = provider.GetService<Engine>();
+            engine.RunHeadless("test-environment");
+            output.Lines.Count.Should().Be(1);
+            output.Lines[0].Should().Be("Single");
+        }
+    }
+}
