6e91aa385d79084e6c1731b4581550c65f9f07a9
Whiteknight <wknight8111@gmail.com>
2020-09-13 09:45:43 -0400

    Add missing files

---
+++ Src/StoneFruit/Handlers/EnvironmentHandler.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Linq;
+using StoneFruit.Execution;
+using StoneFruit.Execution.Arguments;
+using StoneFruit.Execution.Output;
+
+namespace StoneFruit.Handlers
+{
+    [Verb(Name)]
+    public class EnvironmentHandler : IHandler
+    {
+        public const string Name = "env";
+
+        private readonly IOutput _output;
+        private readonly IArguments _args;
+        private readonly EngineState _state;
+        private readonly IEnvironmentCollection _environments;
+        private readonly CommandDispatcher _dispatcher;
+
+        public EnvironmentHandler(IOutput output, IArguments args, EngineState state, IEnvironmentCollection environments, CommandDispatcher dispatcher)
+        {
+            _output = output;
+            _args = args;
+            _state = state;
+            _environments = environments;
+            _dispatcher = dispatcher;
+        }
+
+        public static string Group => HelpHandler.BuiltinsGroup;
+        public static string Description => "List or change environments";
+
+        public static string Usage => $@"{Name} [-list] [-notset] [ <name> | <number>]
+
+{Name}
+Show a prompt to change the current environment, if any are configured.
+
+{Name} -list
+Show a list of all environments
+
+{Name} <envName>
+Change directly to the specified environment
+
+{Name} <number>
+Change directly to the environment at the specified position
+
+{Name} -notset [ <name> | <number> ]?
+Change the current environment, if any are configured but none are set.
+";
+
+        public void Execute()
+        {
+            if (_args.HasFlag("notset"))
+            {
+                if (_environments.Current != null)
+                    return;
+                ChangeEnvironment();
+                return;
+            }
+            if (_args.HasFlag("list"))
+            {
+                ListEnvironments();
+                return;
+            }
+
+            // Otherwise we're switching environments
+            ChangeEnvironment();
+        }
+
+        private void ListEnvironments()
+        {
+            var highlight = new Brush(ConsoleColor.Black, ConsoleColor.Cyan);
+            var envList = _environments.GetNames();
+            for (int i = 0; i < envList.Count; i++)
+            {
+                var index = i + 1;
+                var env = envList[i];
+
+                _output
+                    .Color(ConsoleColor.White).Write(index.ToString())
+                    .Color(ConsoleColor.DarkGray).Write(") ")
+                    .Color(env == _environments.CurrentName ? highlight : ConsoleColor.Cyan).Write(env)
+                    .WriteLine();
+            }
+        }
+
+        private void ChangeEnvironment()
+        {
+            // If invoked with an argument, it is the name or index of an environment. Attempt to set that
+            // environment and exit
+            var target = _args.Shift();
+            if (target.Exists())
+            {
+                var envName = target.AsString();
+                if (envName == _environments.CurrentName)
+                    return;
+                if (!TrySetEnvironment(envName))
+                    throw new EngineBuildException($"Could not set environment {envName}");
+                OnEnvironmentChanged();
+                return;
+            }
+
+            // If we only have a single environment, switch directly to it with no input from the user
+            var environments = _environments.GetNames();
+            if (environments.Count == 1)
+            {
+                _environments.SetCurrent(0);
+                OnEnvironmentChanged();
+                return;
+            }
+
+            PromptUserForEnvironment();
+        }
+
+        private void PromptUserForEnvironment()
+        {
+            // In headless mode we can't prompt, so at this point we just throw an exception
+            if (_state.Headless)
+                throw new Exception("Environment not specified in headless mode");
+
+            // Use the env-list verb to show the list, then prompt the user to make a selection. Loop until
+            // a valid selection is made.
+            while (true)
+            {
+                _output.Color(ConsoleColor.DarkCyan).WriteLine("Please select an environment:");
+                ListEnvironments();
+
+                var envIndex = _output.Prompt("", true, false);
+                if (TrySetEnvironment(envIndex))
+                {
+                    OnEnvironmentChanged();
+                    break;
+                }
+            }
+        }
+
+        private void OnEnvironmentChanged()
+        {
+            var script = _state.EventCatalog.EnvironmentChanged;
+            var args = SyntheticArguments.From(("environment", _environments.CurrentName));
+            _state.Commands.Prepend(script.GetCommands(_dispatcher.Parser, args));
+        }
+
+        private bool TrySetEnvironment(string arg)
+        {
+            // No argument, nothing to do. Fail
+            if (string.IsNullOrEmpty(arg))
+                return false;
+
+            // Argument is a number. Set the environment by index
+            if (arg.All(char.IsDigit))
+            {
+                var asInt = int.Parse(arg) - 1;
+                if (_environments.IsValid(asInt))
+                {
+                    _environments.SetCurrent(asInt);
+                    return true;
+                }
+            }
+
+            // Argument is a name of a valid environment. Set it and exit.
+            if (_environments.IsValid(arg))
+            {
+                _environments.SetCurrent(arg);
+                return true;
+            }
+
+            // Invalid selection, show an error and return failure
+            _output.Color(ConsoleColor.Red).WriteLine($"Unknown environment '{arg}'");
+            return false;
+        }
+    }
+}
---
+++ Src/StoneFruit/Handlers/MetadataHandler.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using StoneFruit.Execution;
+using StoneFruit.Execution.Arguments;
+
+namespace StoneFruit.Handlers
+{
+    [Verb(Name, false)]
+    public class MetadataHandler : IHandler
+    {
+        public const string Name = "metadata";
+        public const string RemoveSubverb = "remove";
+        public const string ListSubverb = "list";
+
+        private readonly EngineState _state;
+        private readonly IArguments _args;
+        private readonly IOutput _output;
+
+        public MetadataHandler(EngineState state, IArguments args, IOutput output)
+        {
+            _state = state;
+            _args = args;
+            _output = output;
+        }
+
+        public static string Group => HelpHandler.BuiltinsGroup;
+        public static string Description => "Work with internal metadata";
+        public static string Usage => @"metadata ...
+
+metadata remove <keys>... 
+Remove a list of metadata items by name
+
+metadata list
+List all available metadata entries
+";
+
+        public void Execute()
+        {
+            var subverb = _args.Shift().Require("Must provide an action").AsString();
+            if (subverb == ListSubverb)
+            {
+                foreach (var kvp in _state.Metadata)
+                {
+                    _output.Color(ConsoleColor.Green).Write(kvp.Key);
+                    _output.Write(": ");
+                    _output.WriteLine(kvp.Value.GetType().Name);
+                }
+                return;
+            }
+            if (subverb == RemoveSubverb)
+            {
+                foreach (var name in _args.GetAllPositionals())
+                    _state.Metadata.Remove(name.AsString());
+                return;
+            }
+        }
+    }
+}
