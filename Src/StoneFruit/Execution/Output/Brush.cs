f947015a50bf3511b0a65c524252ebb90ef60fa0
Whiteknight <wknight8111@gmail.com>
2020-09-16 20:27:00 -0400

    Remove a lot of old Code from Brush
    The Parse and ToSTring code isn't being used and YAGNI
    Also it's killing my code coverage

--- Src/StoneFruit/Execution/Output/Brush.cs
+++ Src/StoneFruit/Execution/Output/Brush.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Globalization;
-using System.Linq;
 using StoneFruit.Utility;
 
 namespace StoneFruit.Execution.Output
@@ -9,7 +7,8 @@ namespace StoneFruit.Execution.Output
     /// Value to represent a foreground/background color combination for writing on the
     /// windows terminal.
     /// </summary>
-    public struct Brush : IEquatable<Brush>
+    public struct Brush
+        : IEquatable<Brush>
     {
         public Brush(byte byteValue)
         {
@@ -27,29 +26,14 @@ namespace StoneFruit.Execution.Output
 
         public static Brush Default => new Brush(ConsoleColor.Gray, ConsoleColor.Black);
 
-        public static Brush Current
-            => new Brush(Console.ForegroundColor, Console.BackgroundColor);
+        public static Brush Current => new Brush(Console.ForegroundColor, Console.BackgroundColor);
 
-        public static implicit operator Brush(ConsoleColor color)
-            => new Brush(color, Console.BackgroundColor);
+        public static implicit operator Brush(ConsoleColor color) => new Brush(color, Console.BackgroundColor);
 
         public readonly void Set()
         {
             Console.ForegroundColor = Foreground;
             Console.BackgroundColor = Background;
-        }
-
-        public override readonly string ToString() => ToString("N");
-
-        public readonly string ToString(string fmt)
-        {
-            if (fmt == "N")
-                return $"{Foreground} on {Background}";
-            if (fmt == "C")
-                return $"{Foreground},{Background}";
-            if (fmt == "B")
-                return ByteValue.ToString("X");
-            throw new ArgumentException("Unknown ToString format", nameof(fmt));
         }
 
         public byte ByteValue { get; }
@@ -79,61 +63,5 @@ namespace StoneFruit.Execution.Output
         public static bool operator ==(Brush a, Brush b) => a.Equals(b);
 
         public static bool operator !=(Brush a, Brush b) => !a.Equals(b);
-
-        public static Brush Parse(string s)
-        {
-            if (TryParse(s, out var palette))
-                return palette;
-            throw new ArgumentException("Value not in a correct format", nameof(s));
-        }
-
-        public static bool TryParse(string s, out Brush palette)
-        {
-            // If length==2 it's a hex byte. Parse that out.
-            if (s.Length == 2)
-            {
-                var byteValue = byte.Parse(s, NumberStyles.HexNumber);
-                palette = new Brush(byteValue);
-                return true;
-            }
-
-            // If length==4 it might be a hex byte with "0x" prefix
-            if (s.Length == 4 && s.StartsWith("0x"))
-            {
-                var byteValue = byte.Parse(s.Substring(2), NumberStyles.HexNumber);
-                palette = new Brush(byteValue);
-                return true;
-            }
-
-            string[] parts = null;
-
-            // Format "ColorName on ColorName" split
-            if (s.Contains(" on "))
-                parts = s.Split(new[] { " on " }, StringSplitOptions.None).Select(x => x.Trim()).ToArray();
-
-            // Format "ColorName,ColorName" split
-            else if (s.Contains(","))
-                parts = s.Split(new[] { "," }, StringSplitOptions.None).Select(x => x.Trim()).ToArray();
-
-            // Otherwise it's nothing we can deal with. Bail out.
-            if (parts == null || parts.Length != 2)
-            {
-                palette = new Brush();
-                return false;
-            }
-
-            try
-            {
-                var foreground = (ConsoleColor)Enum.Parse(typeof(ConsoleColor), parts[0]);
-                var background = (ConsoleColor)Enum.Parse(typeof(ConsoleColor), parts[1]);
-                palette = new Brush(foreground, background);
-                return true;
-            }
-            catch
-            {
-                palette = new Brush();
-                return false;
-            }
-        }
     }
 }
