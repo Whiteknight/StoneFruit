91cf821070e44c436979d7eec5b4d122cd8e662b
Whiteknight <wknight8111@gmail.com>
2020-10-25 09:20:39 -0400

    Add some unit tests for VerbNotFoundException

---
+++ Src/StoneFruit.Tests/Integration/VerbNotFoundExceptionTests.cs
@@ -0,0 +1,20 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using TestUtilities;
+
+namespace StoneFruit.Tests.Integration
+{
+    public class VerbNotFoundExceptionTests
+    {
+        [Test]
+        public void Test()
+        {
+            var output = new TestOutput();
+            var engine = new EngineBuilder()
+                .SetupOutput(o => o.DoNotUseConsole().Add(output))
+                .Build();
+            engine.RunHeadless("does-not-exist");
+            output.Lines[0].Should().StartWith("Verb does-not-exist not found.");
+        }
+    }
+}
