bb6034bdad8e2b280c902dc858d0902cbc73be19
Whiteknight <wknight8111@gmail.com>
2020-10-25 12:44:02 -0400

    Add some tests for Lamar help

---
+++ Src/StoneFruit.Containers.Lamar.Tests/Integration/HelpTests.cs
@@ -0,0 +1,50 @@
+﻿using FluentAssertions;
+using Lamar;
+using Microsoft.Extensions.DependencyInjection;
+using NUnit.Framework;
+using StoneFruit.Execution;
+using TestUtilities;
+
+namespace StoneFruit.Containers.Lamar.Tests.Integration
+{
+    public class HelpTests
+    {
+        [Verb("test-help")]
+        public class TestHandler : IHandler
+        {
+            public void Execute() => throw new System.NotImplementedException();
+
+            public static string Usage => "test the help handler";
+        }
+
+        [Test]
+        public void Help_Test()
+        {
+            var output = new TestOutput();
+            var services = new ServiceRegistry();
+            services.SetupEngine(b => b
+                .SetupOutput(o => o.DoNotUseConsole().Add(output))
+            );
+            var container = new Container(services);
+            var engine = container.GetService<Engine>();
+
+            engine.RunHeadless("help");
+            output.Lines.Should().Contain("test-help");
+        }
+
+        [Test]
+        public void Help_Usage_Test()
+        {
+            var output = new TestOutput();
+            var services = new ServiceRegistry();
+            services.SetupEngine(b => b
+                .SetupOutput(o => o.DoNotUseConsole().Add(output))
+            );
+            var container = new Container(services);
+            var engine = container.GetService<Engine>();
+
+            engine.RunHeadless("help test-help");
+            output.Lines.Should().Contain("test the help handler");
+        }
+    }
+}
