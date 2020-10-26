2458707fdbdf1f7f1ba3bb43de3a0c2853541c50
Whiteknight <wknight8111@gmail.com>
2020-10-26 19:07:48 -0400

    Finish implementing the Autofac container bindings and add it to build/publish scripts

--- Src/StoneFruit.Containers.Autofac/AutofacHandlerSource.cs
+++ Src/StoneFruit.Containers.Autofac/AutofacHandlerSource.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using Autofac;
 using Autofac.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
 using StoneFruit.Execution;
 using StoneFruit.Utility;
 
@@ -10,21 +11,62 @@ namespace StoneFruit.Containers.Autofac
 {
     public class AutofacHandlerSource : IHandlerSource
     {
-        private readonly IContainer _container;
-        private readonly VerbTrie<Type> _types;
+        private readonly IServiceProvider _serviceProvider;
+        private readonly VerbTrie<Type> _handlers;
 
         public AutofacHandlerSource(IServiceProvider provider, IVerbExtractor verbExtractor)
         {
-            //_container = container;
-            var handlerTypes = (provider as AutofacServiceProvider).LifetimeScope.ComponentRegistry.Registrations
+            _serviceProvider = provider;
+            _handlers = (provider as AutofacServiceProvider).LifetimeScope.ComponentRegistry.Registrations
                 .Where(r => typeof(IHandlerBase).IsAssignableFrom(r.Activator.LimitType))
                 .Select(r => r.Activator.LimitType)
-                .ToList();
-
+                .Where(t => t != null && t.IsClass && !t.IsAbstract)
+                .Distinct()
+                .SelectMany(commandType => verbExtractor
+                    .GetVerbs(commandType)
+                    .Select(verb => (Verb: verb, Type: commandType))
+                )
+                .ToVerbTrie(x => x.Verb, x => x.Type);
         }
 
-        public IEnumerable<IVerbInfo> GetAll() => throw new NotImplementedException();
-        public IVerbInfo GetByName(Verb verb) => throw new NotImplementedException();
-        public IHandlerBase GetInstance(IArguments arguments, CommandDispatcher dispatcher) => throw new NotImplementedException();
+        public IEnumerable<IVerbInfo> GetAll() => _handlers.GetAll().Select(kvp => new VerbInfo(kvp.Key, kvp.Value));
+
+        public IVerbInfo GetByName(Verb verb)
+        {
+            var type = _handlers.Get(verb);
+            if (type == null)
+                return null;
+            return new VerbInfo(verb, type);
+        }
+
+        public IHandlerBase GetInstance(IArguments arguments, CommandDispatcher dispatcher)
+        {
+            var type = _handlers.Get(arguments);
+            return type == null ? null : ResolveHandler(type);
+        }
+
+        private IHandlerBase ResolveHandler(Type type)
+        {
+            using var scope = _serviceProvider.CreateScope();
+            var instance = scope.ServiceProvider.GetService(type);
+            return instance as IHandlerBase;
+        }
+
+        private class VerbInfo : IVerbInfo
+        {
+            private readonly Type _type;
+
+            public VerbInfo(Verb verb, Type type)
+            {
+                _type = type;
+                Verb = verb;
+            }
+
+            public Verb Verb { get; }
+            public string Description => _type.GetDescription();
+            public string Usage => _type.GetUsage();
+            public string Group => _type.GetGroup();
+            public bool ShouldShowInHelp => _type.ShouldShowInHelp(Verb);
+        }
     }
 }
--- _allProjects.sh
+++ _allProjects.sh
@@ -1,1 +1,1 @@
-all_projects=("StoneFruit" "StoneFruit.Containers.Lamar" "StoneFruit.Containers.Microsoft" "StoneFruit.Containers.StructureMap");
+all_projects=("StoneFruit" "StoneFruit.Containers.Autofac" "StoneFruit.Containers.Lamar" "StoneFruit.Containers.Microsoft" "StoneFruit.Containers.StructureMap");
