8642da50b064a495793e11a5257ae83f83df03d9
Whiteknight <wknight8111@gmail.com>
2021-02-13 15:16:43 -0500

    Move Engine to the root namespace, to simplify use in downstream projects

--- Src/StoneFruit/Engine.cs
+++ Src/StoneFruit/Engine.cs
@@ -1,24 +1,13 @@
 ﻿using System;
+using StoneFruit.Execution;
 using StoneFruit.Execution.Arguments;
 using StoneFruit.Execution.CommandSources;
 using StoneFruit.Execution.Handlers;
 using StoneFruit.Handlers;
 using StoneFruit.Utility;
 
-namespace StoneFruit.Execution
+namespace StoneFruit
 {
-    public class EngineAccessor
-    {
-        private Engine? _engine;
-
-        public Engine Engine => _engine ?? throw new InvalidOperationException("Cannot access engine because one has not been created");
-
-        public void SetEngine(Engine engine)
-        {
-            _engine = engine;
-        }
-    }
-
     /// <summary>
     /// The execution core. Provides a run loop to receive user commands and execute them
     /// </summary>
---
+++ Src/StoneFruit/Execution/EngineAccessor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using StoneFruit.Execution.Arguments;
+using StoneFruit.Execution.CommandSources;
+using StoneFruit.Execution.Handlers;
+using StoneFruit.Handlers;
+using StoneFruit.Utility;
+
+namespace StoneFruit.Execution
+{
+    public class EngineAccessor
+    {
+        private Engine? _engine;
+
+        public Engine Engine => _engine ?? throw new InvalidOperationException("Cannot access engine because one has not been created");
+
+        public void SetEngine(Engine engine)
+        {
+            _engine = engine;
+        }
+    }
+}
