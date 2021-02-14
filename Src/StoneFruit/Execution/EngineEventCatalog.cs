bff2b7f2c961dd30cee0fddba0555aa988b2512b
Whiteknight <wknight8111@gmail.com>
2021-02-13 20:29:22 -0500

    Cleanup EngineEventCatalog, combine declarations and data instantiations

--- Src/StoneFruit/Execution/EngineEventCatalog.cs
+++ Src/StoneFruit/Execution/EngineEventCatalog.cs
@@ -7,20 +7,34 @@ namespace StoneFruit.Execution
     /// </summary>
     public class EngineEventCatalog
     {
-        public EngineEventCatalog()
-        {
-            // When there's an unhandled exception caught by the engine
-            EngineError = new EventScript(
-                $"{EchoHandler.Name} color=Red ['message']",
-                $"{EchoHandler.Name} ['stacktrace']"
+        /// <summary>
+        /// The engine has been run in headless mode without a command.
+        /// </summary>
+        public EventScript HeadlessNoArgs { get; } = new EventScript(
+                // Has argument 'exitcode' with the intended exit code
+                $"{EchoHandler.Name} No command provided",
+
+                // Call 'exit' so we can set an explicit error exit code
+                $"{ExitHandler.Name} ['exitcode']"
             );
 
-            // Events when the engine starts and then stops headless mode
-            EngineStartHeadless = new EventScript();
-            EngineStopHeadless = new EventScript();
+        /// <summary>
+        /// The engine has started in headless mode. This script is run, followed by the
+        /// input command, and then the EngineStopHeadless script.
+        /// </summary>
+        public EventScript EngineStartHeadless { get; } = new EventScript();
 
-            // Events when the engine starts and stops interactive mode
-            EngineStartInteractive = new EventScript(
+        /// <summary>
+        /// The engine has stopped headless mode. This script is executed after
+        /// EngineStartHeadless script and the user command.
+        /// </summary>
+        public EventScript EngineStopHeadless { get; } = new EventScript();
+
+        /// <summary>
+        /// The engine has started in interactive mode. This script will execute, then the
+        /// user will be shown a REPL prompt.
+        /// </summary>
+        public EventScript EngineStartInteractive { get; } = new EventScript(
                 // Call the env-change command to make sure we have an environment set
                 $"{EnvironmentHandler.Name} -notset",
 
@@ -30,92 +44,56 @@ namespace StoneFruit.Execution
                 $"{EchoHandler.Name} ':'"
             );
 
-            // Event when the environment has been successfully changed
-            // Has argument 'environment' with name of new environment
-            EnvironmentChanged = new EventScript(
-                $"{EchoHandler.Name} -noheadless Environment changed to ['environment']"
-            );
-
-            // We're executing basic help command headlessly
-            // Has argument 'exitcode' with the intended exit code
-            HeadlessHelp = new EventScript(
-                $"{HelpHandler.Name}",
-
-                // Call 'exit' explicitly so we can set the exit code
-                $"{ExitHandler.Name} ['exitcode']"
-            );
-
-            // Attempt to enter headless mode without providing any arguments
-            // Has argument 'exitcode' with the intended exit code
-            HeadlessNoArgs = new EventScript(
-                $"{EchoHandler.Name} No command provided",
-
-                // Call 'exit' so we can set an explicit error exit code
-                $"{ExitHandler.Name} ['exitcode']"
-            );
-
-            // Attempt to execute an unknown verb
-            // Has argument 'verb' with the name of the verb
-            VerbNotFound = new EventScript(
+        /// <summary>
+        /// There is no handler for the specified verb.
+        /// </summary>
+        public EventScript VerbNotFound { get; } = new EventScript(
+                // Has argument 'verb' with the name of the verb
                 $"{EchoHandler.Name} Verb ['verb'] not found. Please check your spelling or help output and try again.",
                 $"{HelpHandler.Name} -startswith ['verb']"
             );
 
-            MaximumHeadlessCommands = new EventScript(
-                $"{EchoHandler.Name} Maximum ['limit'] commands executed without user input. Terminating runloop.",
-                $"{ExitHandler.Name} ['exitcode']"
-            );
-        }
-
-        /// <summary>
-        /// The engine has been run in headless mode without a command.
-        /// </summary>
-        public EventScript HeadlessNoArgs { get; }
-
-        /// <summary>
-        /// The engine has started in headless mode. This script is run, followed by the
-        /// input command, and then the EngineStopHeadless script.
-        /// </summary>
-        public EventScript EngineStartHeadless { get; }
-
-        /// <summary>
-        /// The engine has stopped headless mode. This script is executed after
-        /// EngineStartHeadless script and the user command.
-        /// </summary>
-        public EventScript EngineStopHeadless { get; }
-
-        /// <summary>
-        /// The engine has started in interactive mode. This script will execute, then the
-        /// user will be shown a REPL prompt.
-        /// </summary>
-        public EventScript EngineStartInteractive { get; }
-
-        /// <summary>
-        /// There is no handler for the specified verb.
-        /// </summary>
-        public EventScript VerbNotFound { get; }
-
         /// <summary>
         /// The environment has been changed
         /// </summary>
-        public EventScript EnvironmentChanged { get; }
+        public EventScript EnvironmentChanged { get; } = new EventScript(
+                // Has argument 'environment' with name of new environment
+                $"{EchoHandler.Name} -noheadless Environment changed to ['environment']"
+            );
 
         /// <summary>
         /// "help" has been executed in headless mode. Can be used to show more detail than
-        /// a simple help handle. Notice that this script may be run without a valid
+        /// a simple help call. Notice that this script may be run without a valid
         /// environment set.
         /// </summary>
-        public EventScript HeadlessHelp { get; }
+        public EventScript HeadlessHelp { get; } = new EventScript(
+                $"{HelpHandler.Name}",
+
+                // Call 'exit' explicitly so we can set the exit code
+                // Has argument 'exitcode' with the intended exit code
+                $"{ExitHandler.Name} ['exitcode']"
+            );
 
         /// <summary>
         /// An unhandled exception has been received by the engine. The exception may have
         /// come from engine internals or from within one of the handlers.
         /// </summary>
-        public EventScript EngineError { get; }
+        public EventScript EngineError { get; } = new EventScript(
+                // Contains the exception message and exception stack trace as arguments
+                $"{EchoHandler.Name} color=Red ['message']",
+                $"{EchoHandler.Name} ['stacktrace']"
+            );
 
         /// <summary>
         /// A maximum number of commands have been executed without user input.
         /// </summary>
-        public EventScript MaximumHeadlessCommands { get; }
+        public EventScript MaximumHeadlessCommands { get; } = new EventScript(
+                // Has argument 'limit' with the number of commands specified in the Settings, and
+                // 'exitcode' with the intended exit code value
+                // Notice that the command counter is reset before executing this script, but it
+                // is not disabled. This script cannot contain more than limit entries
+                $"{EchoHandler.Name} Maximum ['limit'] commands executed without user input. Terminating runloop.",
+                $"{ExitHandler.Name} ['exitcode']"
+            );
     }
 }
