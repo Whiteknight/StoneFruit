944c1ddf7889d88e96c3a1d90aa7149c9a28650f
Whiteknight <wknight8111@gmail.com>
2021-09-02 11:08:11 -0400

    Update build and publish scripts to include Unity adaptor lib

--- build.sh
+++ build.sh
@@ -10,3 +10,6 @@ dotnet pack Src/StoneFruit.Containers.Lamar/StoneFruit.Containers.Lamar.csproj -
 dotnet build -f netstandard2.0 Src/StoneFruit.Containers.Microsoft/StoneFruit.Containers.Microsoft.csproj --configuration Release
 dotnet pack Src/StoneFruit.Containers.Microsoft/StoneFruit.Containers.Microsoft.csproj --configuration Release --no-build --no-restore
 
+dotnet build -f netstandard2.0 Src/StoneFruit.Containers.Unity/StoneFruit.Containers.Unity.csproj --configuration Release
+dotnet pack Src/StoneFruit.Containers.Unity/StoneFruit.Containers.Unity.csproj --configuration Release --no-build --no-restore
+
--- publish.sh
+++ publish.sh
@@ -3,4 +3,5 @@ dotnet nuget push Src/StoneFruit.Containers.StructureMap/bin/Release/StoneFruit.
 dotnet nuget push Src/StoneFruit.Containers.Lamar/bin/Release/StoneFruit.Containers.Lamar.$1.nupkg --source https://www.nuget.org/api/v2/package
 dotnet nuget push Src/StoneFruit.Containers.Ninject/bin/Release/StoneFruit.Containers.Ninject.$1.nupkg --source https://www.nuget.org/api/v2/package
 dotnet nuget push Src/StoneFruit.Containers.Microsoft/bin/Release/StoneFruit.Containers.Microsoft.$1.nupkg --source https://www.nuget.org/api/v2/package
+dotnet nuget push Src/StoneFruit.Containers.Unity/bin/Release/StoneFruit.Containers.Unity.$1.nupkg --source https://www.nuget.org/api/v2/package
 
