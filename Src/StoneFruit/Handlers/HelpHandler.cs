c70674fca46abf19a5221e17d8c4b1a8f7c51e93
Whiteknight <wknight8111@gmail.com>
2022-10-15 07:46:32 -0400

    Fixes and cleanups for HelpHandler
    -startswith respects -showall
    -startswith doesn't throw an error if no verbs match
    Fix indenting of Description text when the verb has no group
    Fix breaking of Description into lines
    Break up a few methods and do some basic cleanup/refactoring

--- Src/StoneFruit/Handlers/HelpHandler.cs
+++ Src/StoneFruit/Handlers/HelpHandler.cs
@@ -9,6 +9,8 @@ namespace StoneFruit.Handlers
     public class HelpHandler : IHandler
     {
         public const string Name = "help";
+        public const string FlagShowAll = "showall";
+        public const string FlagStartsWith = "startswith";
         public const string BuiltinsGroup = "__STONEFRUIT_BUILTIN";
 
         private readonly IOutput _output;
@@ -26,36 +28,37 @@ namespace StoneFruit.Handlers
 
         public static string Description => "List all commands or get detailed information for a single command";
 
-        public static string Usage => @"help [-showall]
-    Get overview information for all available verbs. The verb Command class must implement
+        public static string Usage => $@"{Name} [ -{FlagShowAll} ]
+    Get overview information for all available verbs. The verb Command class should implement
 
-        public static string Description {{ get; }}
+        public static string {nameof(Description)} {{ get; }}
 
     The help command by default hides some internal commands which are not necessary for normal
-    user interaction. To see all commands, use the -showall flag.
+    user interaction. To see all commands, use the -{FlagShowAll} flag.
 
-help <verb>
-    Get detailed help information for the given verb. The verb Command class must implement
+{Name} <verb>
+    Get detailed help information for the given verb. The verb Command class should implement
 
-        public static string Usage {{ get; }}
+        public static string {nameof(Usage)} {{ get; }}
 
-help -startswith <word>
+{Name} -{FlagStartsWith} <word>
     Get overview information for all handlers whose verbs start with the given word. The verb
-    Command class must implement the Description property, described above.
+    Command class should implement the {nameof(Description)} property, described above.
 ";
 
         public void Execute()
         {
             var verb = _args.GetAllPositionals().Select(p => p.AsString()).ToArray();
+            var showAll = _args.HasFlag(FlagShowAll);
             if (verb.Length == 0)
             {
-                GetOverview(_args.HasFlag("showall"));
+                GetOverview(showAll);
                 return;
             }
 
-            if (_args.HasFlag("startswith"))
+            if (_args.HasFlag(FlagStartsWith))
             {
-                GetOverviewStartingWith(verb[0]);
+                GetOverviewStartingWith(verb[0], showAll);
                 return;
             }
 
@@ -72,17 +75,19 @@ help -startswith <word>
 
         private void GetOverview(bool showAll)
         {
-            var infoList = _commands.GetAll();
-            if (!showAll)
-                infoList = infoList.Where(i => i.ShouldShowInHelp);
+            var infoList = _commands.GetAll()
+                .Where(i => showAll || i.ShouldShowInHelp);
             ShowOverview(infoList);
         }
 
-        private void GetOverviewStartingWith(string word)
+        private void GetOverviewStartingWith(string word, bool showAll)
         {
             var infoList = _commands.GetAll()
-                .Where(i => i.ShouldShowInHelp && i.Verb[0].StartsWith(word, StringComparison.OrdinalIgnoreCase));
-            ShowOverview(infoList);
+                .Where(i => (showAll || i.ShouldShowInHelp)
+                    && i.Verb[0].StartsWith(word, StringComparison.OrdinalIgnoreCase)
+                );
+            if (infoList.Any())
+                ShowOverview(infoList);
         }
 
         private void ShowOverview(IEnumerable<IVerbInfo> infoList)
@@ -94,7 +99,13 @@ help -startswith <word>
             int descStartColumn = maxCommandLength + 4;
             var blankPrefix = new string(' ', descStartColumn);
             int maxDescLineLength = GetConsoleWidth() - descStartColumn;
+            OutputOverviewGroups(infoGroups, descStartColumn, blankPrefix, maxDescLineLength);
 
+            _output.WriteLine($"Type '{Name} <verb>' to get more information, if available.");
+        }
+
+        private void OutputOverviewGroups(Dictionary<string, List<IVerbInfo>> infoGroups, int descStartColumn, string blankPrefix, int maxDescLineLength)
+        {
             if (infoGroups.ContainsKey(""))
                 OutputVerbList("", infoGroups[""], descStartColumn, maxDescLineLength, blankPrefix);
 
@@ -103,8 +114,6 @@ help -startswith <word>
 
             if (infoGroups.ContainsKey(BuiltinsGroup) && infoGroups[BuiltinsGroup].Any())
                 OutputVerbList("Built-In Verbs", infoGroups[BuiltinsGroup], descStartColumn, maxDescLineLength, blankPrefix);
-
-            _output.WriteLine("Type 'help <command-name>' to get more information, if available.");
         }
 
         private static int GetConsoleWidth()
@@ -121,46 +130,71 @@ help -startswith <word>
 
         private void OutputVerbList(string groupName, IEnumerable<IVerbInfo> verbInfos, int descStartColumn, int maxDescLineLength, string blankPrefix)
         {
-            string padLeft = "";
-            if (!string.IsNullOrEmpty(groupName))
-            {
-                padLeft = "  ";
-                _output
-                    .Color(ConsoleColor.Blue)
-                    .WriteLine(groupName);
-            }
+            // Write the group name (if we have one)
+            string padLeft = OutputGroupNameAndSetupPadding(groupName);
 
+            // Foreach item in the group output the padding, the verb, then the description
             foreach (var info in verbInfos)
             {
                 _output
                     .Color(ConsoleColor.Green)
                     .Write(padLeft)
                     .Write(info.Verb)
-                    .Write(new string(' ', descStartColumn - info.Verb.ToString().Length));
-                var desc = info.Description;
-                var lines = GetDescriptionLines(desc, maxDescLineLength);
-                if (lines.Count == 0)
-                {
-                    _output.WriteLine();
-                    continue;
-                }
-
-                _output.WriteLine(lines[0]);
-                foreach (var line in lines.Skip(1))
-                    _output.WriteLine($"{blankPrefix}{line}");
+                    .Write(new string(' ', descStartColumn - (info.Verb.ToString() + padLeft).Length));
+                OutputDescriptionLines(maxDescLineLength, blankPrefix, info.Description);
             }
         }
 
-        private static List<string> GetDescriptionLines(string desc, int max)
+        private void OutputDescriptionLines(int maxDescLineLength, string blankPrefix, string desc)
+        {
+            var lines = GetDescriptionLines(desc, maxDescLineLength);
+            if (lines.Count == 0)
+            {
+                _output.WriteLine();
+                return;
+            }
+
+            // For the first line we are already indented because we have output the padding and
+            // verb already. So just write the line. For subsequent lines we need to output the
+            // blankPrefix first to line everything up
+            _output.WriteLine(lines[0]);
+            foreach (var line in lines.Skip(1))
+                _output.WriteLine($"{blankPrefix}{line}");
+        }
+
+        private string OutputGroupNameAndSetupPadding(string groupName)
+        {
+            // If there is no group name do nothing and return nothing
+            if (string.IsNullOrEmpty(groupName))
+                return string.Empty;
+
+            // Otherwise, output the group name and return a padding value that we
+            // use to indent all items in this group
+            _output
+                .Color(ConsoleColor.Blue)
+                .WriteLine(groupName);
+            return "  ";
+        }
+
+        private static IReadOnlyList<string> GetDescriptionLines(string desc, int max)
         {
             if (max <= 0 || string.IsNullOrEmpty(desc))
-                return new List<string>();
+                return Array.Empty<string>();
+
+            if (desc.Length < max)
+                return new[] { desc };
+
             var list = new List<string>();
             int start = 0;
             while (start < desc.Length)
             {
-                var available = Math.Min(start + max, desc.Length);
-                var line = desc.Substring(start, available);
+                if (desc.Length - start < max)
+                {
+                    list.Add(desc.Substring(start, desc.Length - start));
+                    break;
+                }
+
+                var line = desc.Substring(start, max);
                 start += max;
                 list.Add(line);
             }
