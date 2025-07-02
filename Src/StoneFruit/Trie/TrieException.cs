52045a153d65e6b35178d352cff3ddb98ad062e1
Whiteknight <wknight8111@gmail.com>
2025-07-02 15:56:12 -0400

    fix some vulnerable package warnings

--- Src/StoneFruit/Trie/TrieException.cs
+++ Src/StoneFruit/Trie/TrieException.cs
@@ -1,16 +1,18 @@
-﻿namespace StoneFruit.Trie;
+﻿using System;
+
+namespace StoneFruit.Trie;
 
 /// <summary>
-/// Exception that is thrown during trie operations
+/// Exception that is thrown during trie operations.
 /// </summary>
-[System.Serializable]
+[Serializable]
 public class TrieException : System.Exception
 {
-    public TrieException(string message) : base(message)
+    public TrieException(string message)
+        : base(message)
     {
     }
 
     public static TrieException NoArguments()
         => new TrieException("Could not look up an appropriate handler, no verbs were provided");
 }
-
