1c1a27705c2ea485ff15ebfebadba5112fc8f42c
Whiteknight <wknight8111@gmail.com>
2025-07-12 15:38:07 -0400

    Move attributes together into a single file

---
+++ Src/StoneFruit/Attributes.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace StoneFruit;
+
+/// <summary>
+/// Specifies a human-readable description of the handler class or method, for display in the
+/// help output.
+/// </summary>
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = false, Inherited = false)]
+public class DescriptionAttribute : Attribute
+{
+    public DescriptionAttribute(string description)
+    {
+        Description = description;
+    }
+
+    public string Description { get; }
+}
+
+/// <summary>
+/// Specifies a Group for a Handler class or method, when it appears in the help output.
+/// </summary>
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = false, Inherited = false)]
+public class GroupAttribute : Attribute
+{
+    public GroupAttribute(string group)
+    {
+        Group = group;
+    }
+
+    public string Group { get; }
+}
+
+/// <summary>
+/// Specifies a usage string to display in the help output for the class or method.
+/// </summary>
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = false, Inherited = false)]
+public class UsageAttribute : Attribute
+{
+    public UsageAttribute(string usage)
+    {
+        Usage = usage;
+    }
+
+    public string Usage { get; }
+}
+
+/// <summary>
+/// Specify a custom verb to use instead of automatically deriving the verb from the name of
+/// the class or method.
+/// </summary>
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = true, Inherited = false)]
+public class VerbAttribute : Attribute
+{
+    public VerbAttribute(params string[] verb)
+    {
+        Verb = verb;
+    }
+
+    public Verb Verb { get; }
+
+    public bool Hide { get; set; }
+}
