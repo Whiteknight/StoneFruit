3bbf0bbe9ea5b61370af4c47af654a0585af7ae3
Whiteknight <wknight8111@gmail.com>
2025-08-09 13:33:07 -0400

    publish.sh gets the current version from the .csproj, if none is specified

--- publish.sh
+++ publish.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+fileversion=$(grep "<Version>" "Src/StoneFruit/StoneFruit.csproj" | sed 's/.*<Version>\(.*\)<\/Version>/\1/')
+version=${1:-$fileversion} 
 dotnet nuget push Src/StoneFruit/bin/Release/StoneFruit.$1.nupkg --source https://www.nuget.org/api/v2/package
 dotnet nuget push Src/StoneFruit/bin/Release/StoneFruit.$1.snupkg --source https://www.nuget.org/api/v2/package
