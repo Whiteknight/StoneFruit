ecf5e201130adc17da1a63418bbe04cf3f22ed01
Whiteknight <wknight8111@gmail.com>
2025-08-09 12:52:40 -0400

    Fix scan script

--- Scripts/scansq.sh
+++ Scripts/scansq.sh
@@ -6,6 +6,9 @@
 # URL to Sonarqube. If nginx is working we can use the first. "if"
 SONARQUBE_URL=http://localhost:9000
 
+rm -rf Src/StoneFruit.SpecTests/TestResults
+rm -rf Src/StoneFruit.Tests/TestResults
+
 dotnet sonarscanner begin \
     -d:sonar.token="$SQ_STONEFRUIT_KEY" \
     -d:sonar.host.url="$SONARQUBE_URL" \
@@ -15,7 +18,7 @@ dotnet sonarscanner begin \
 dotnet build --no-incremental Src/StoneFruit.sln
 
 #dotnet test --collect:"XPlat Code Coverage;Format=opencover"  Src/StoneFruit.sln
-dotnet test /p:CollectCoverage=true /p:CoverletOutputFormat=opencover Src/StoneFruit.sln
+dotnet test --collect:"XPlat Code Coverage;Format=opencover"  Src/StoneFruit.Tests/StoneFruit.Tests.csproj
+dotnet test --collect:"XPlat Code Coverage;Format=opencover"  Src/StoneFruit.SpecTests/StoneFruit.SpecTests.csproj
 
 dotnet sonarscanner end -d:sonar.token="$SQ_STONEFRUIT_KEY"
-#rm coverage.xml
