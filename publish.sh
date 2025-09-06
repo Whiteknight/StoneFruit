a45995f3dfedeb408af78008ae73f7782c0086a6
Whiteknight <wknight8111@gmail.com>
2025-09-06 07:34:09 -0400

    Fix publish.sh script

--- publish.sh
+++ publish.sh
@@ -2,5 +2,5 @@
 
 fileversion=$(grep "<Version>" "Src/StoneFruit/StoneFruit.csproj" | sed 's/.*<Version>\(.*\)<\/Version>/\1/')
 version=${1:-$fileversion} 
-dotnet nuget push Src/StoneFruit/bin/Release/StoneFruit.$1.nupkg --source https://www.nuget.org/api/v2/package
-dotnet nuget push Src/StoneFruit/bin/Release/StoneFruit.$1.snupkg --source https://www.nuget.org/api/v2/package
+dotnet nuget push Src/StoneFruit/bin/Release/StoneFruit.$version.nupkg --source https://www.nuget.org/api/v2/package
+dotnet nuget push Src/StoneFruit/bin/Release/StoneFruit.$version.snupkg --source https://www.nuget.org/api/v2/package
